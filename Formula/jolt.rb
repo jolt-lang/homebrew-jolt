7d2c83b17fcd091a339f376aadca52ee29e15abe
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2026-07-04 17:23:46 -0400

    jolt 0.1.6

--- Formula/jolt.rb
+++ Formula/jolt.rb
@@ -5,8 +5,8 @@ class Jolt < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/jolt-lang/jolt/releases/download/v0.1.5/joltc-v0.1.5-aarch64-macos.tar.gz"
-      sha256 "4cebdbcd6df2cf72478859cbed9afc70c3c69e98ad0ceb30e7115ab28b948e66"
+      url "https://github.com/jolt-lang/jolt/releases/download/v0.1.6/joltc-v0.1.6-aarch64-macos.tar.gz"
+      sha256 "47753300d8a852940ab6c54bed9228bab946627468f7b6fde209794135c771a8"
     end
     # No Intel macOS bottle: GitHub retired the macos-13 Intel runner. Intel
     # Macs build joltc from source (needs Chez Scheme + a C compiler).
@@ -14,8 +14,8 @@ class Jolt < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/jolt-lang/jolt/releases/download/v0.1.5/joltc-v0.1.5-x86_64-linux.tar.gz"
-      sha256 "a5a253b07989926c35465829144ac640b4a5baa926616f12ad7fc54a2381c26b"
+      url "https://github.com/jolt-lang/jolt/releases/download/v0.1.6/joltc-v0.1.6-x86_64-linux.tar.gz"
+      sha256 "fd62b46f53e9027605bbc56b91386469e664c3026877b5a3deceef819638be6c"
     end
   end
 
